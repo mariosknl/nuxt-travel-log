d0d62f3e130b1f77fcd8201594a7a00995d741a9
mariosknl <mariosknl@outlook.com>
2025-05-28 08:41:08 +0300

    :sparkles: add theme toggle

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,9 @@
   <div class="hero bg-base-300 container mx-auto mt-4">
     <div class="hero-content text-center min-h-96">
       <div class="max-w-md">
-        <h1 class="text-5xl font-bold">Travel Log</h1>
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
         <p class="py-6">
           Keep track of your travels and adventures with this simple travel log
           app.
