48547848f7d65dbeab3b3531941a5ddf8c2798f9
Marios Kanellopoulos <mariosknl@outlook.com>
2025-06-04 13:52:03 +0300

    :sparkles: add landing page (#51)

---
+++ components/app/nav-bar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-lg">Travel Log</NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <button class="btn btn-accent">
+        Sign In with Github <Icon name="tabler:brand-github" size="24" />
+      </button>
+    </div>
+  </div>
+</template>
