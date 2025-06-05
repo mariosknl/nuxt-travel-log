9c52e8241e925889a447584f36bf204694c34a43
mariosknl <mariosknl@outlook.com>
2025-06-05 08:21:13 +0300

    :sparkles: logout functionality

--- components/auth-button.vue
+++ components/auth-button.vue
@@ -3,7 +3,37 @@ const authStore = useAuthStore();
 </script>
 
 <template>
+  <div
+    v-if="!authStore.loading && authStore.user"
+    class="dropdown dropdown-end"
+  >
+    <div
+      tabindex="0"
+      role="button"
+      class="btn m-1"
+    >
+      <div v-if="authStore.user.image" class="avatar">
+        <div class="w-8 rounded-full">
+          <img :src="authStore.user.image" :alt="authStore.user.name">
+        </div>
+      </div>
+      {{ authStore.user.name }}
+    </div>
+    <ul
+      tabindex="0"
+      class="dropdown-content menu bg-base-200 rounded-box w-52 p-2 shadow-sm"
+    >
+      <li>
+        <NuxtLink to="/sign-out">
+          <Icon name="tabler:logout-2" size="24" />
+          Sign out
+        </NuxtLink>
+      </li>
+      <li><a>Item 2</a></li>
+    </ul>
+  </div>
   <button
+    v-else
     :disabled="authStore.loading"
     class="btn btn-accent"
     @click="authStore.signIn"
