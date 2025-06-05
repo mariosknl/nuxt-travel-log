64fe20237c4c6817f63d19359a6effbd7b7e17f2
mariosknl <mariosknl@outlook.com>
2025-06-05 08:38:17 +0300

    :sparkles: applying middleware

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+</script>
+
 <template>
   <div class="hero bg-base-300 container mx-auto mt-4">
     <div class="hero-content text-center min-h-96">
@@ -9,7 +13,14 @@
           Keep track of your travels and adventures with this simple travel log
           app.
         </p>
-        <AuthButton />
+        <AuthButton v-if="!authStore.user" />
+        <NuxtLink
+          v-else
+          to="/dashboard"
+          class="btn btn-primary"
+        >
+          Start Logging
+        </NuxtLink>
       </div>
     </div>
   </div>
