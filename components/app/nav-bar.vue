67df2db29783a8a37708a1c10febd34932e08a2b
mariosknl <mariosknl@outlook.com>
2025-06-05 07:20:32 +0300

    :sparkles: github integration

--- components/app/nav-bar.vue
+++ components/app/nav-bar.vue
@@ -7,9 +7,7 @@
     </div>
     <div class="navbar-end">
       <AppThemeToggle />
-      <button class="btn btn-accent">
-        Sign In with Github <Icon name="tabler:brand-github" size="24" />
-      </button>
+      <AuthButton />
     </div>
   </div>
 </template>
---
+++ components/auth-button.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+</script>
+
+<template>
+  <button
+    :disabled="authStore.loading"
+    class="btn btn-accent"
+    @click="authStore.signIn"
+  >
+    Sign in with Github
+    <span v-if="authStore.loading" class="loading loading-spinner loading-md" />
+    <Icon
+      v-else
+      name="tabler:brand-github"
+      size="24"
+    />
+  </button>
+</template>
